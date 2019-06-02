03b24de1d13b006465d2b6ebbd5d692a71018c82
makienkok <kirillmakienko@gmail.com>
2019-06-03 02:33:40 +0300

    hibernate add

---
+++ TasksQuery/src/main/java/com/tasksquery/repositories/TaskRepository.java
@@ -0,0 +1,12 @@
+package com.tasksquery.repositories;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tasksquery.models.Task;
+
+@Repository
+public interface TaskRepository extends PagingAndSortingRepository<Task, Integer> 
+{
+	
+}
